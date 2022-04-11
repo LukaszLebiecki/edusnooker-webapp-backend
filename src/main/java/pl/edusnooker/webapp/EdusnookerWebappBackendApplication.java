ccd0bb79dbccae10de23be920ab3cb458ba49da4
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2022-04-11 14:46:06 +0200

    first commit

---
+++ src/main/java/pl/edusnooker/webapp/EdusnookerWebappBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.edusnooker.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusnookerWebappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdusnookerWebappBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edusnooker/webapp/EdusnookerWebappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edusnooker.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusnookerWebappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
