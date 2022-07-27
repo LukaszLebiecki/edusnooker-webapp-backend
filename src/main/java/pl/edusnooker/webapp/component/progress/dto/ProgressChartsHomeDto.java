cbb67f7bb792193cbb27aa42926def3fce4d3c1c
Lebiecki <lukasz.lebiecki@gmail.com>
2022-07-28 00:06:46 +0200

    add two new exercises

--- src/main/java/pl/edusnooker/webapp/component/progress/dto/ProgressChartsHomeDto.java
+++ src/main/java/pl/edusnooker/webapp/component/progress/dto/ProgressChartsHomeDto.java
@@ -5,4 +5,20 @@ import lombok.Data;
 @Data
 public class ProgressChartsHomeDto {
     private int[] chartsHome;
+
+    private int[] pointsScoredToYear;
+    private int[] exercisesPerformedToYear;
+    private int[] exercisesCompletedToYear;
+
+    private int[] pointsScoredToMonth;
+    private int[] exercisesPerformedToMonth;
+    private int[] exercisesCompletedToMonth;
+
+    private int[] pointsScoredToWeek;
+    private int[] exercisesPerformedToWeek;
+    private int[] exercisesCompletedToWeek;
+
+    private int[] pointsScoredToHour;
+    private int[] exercisesPerformedToHour;
+    private int[] exercisesCompletedToHour;
 }
