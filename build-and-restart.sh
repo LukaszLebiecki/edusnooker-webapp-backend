d8cc8384a9b96a1dd67f50c6e0412ea2470329cf
LukaszLebiecki <lukasz.lebiecki@gmail.com>
2022-09-10 20:18:55 +0200

    mysql-docker-001

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t edusnooker .
 docker stop edusnooker
 docker rm edusnooker
-docker run -d -p 8080:8080 --name edusnooker -e SPRING_PROFILES_ACTIVE=prod edusnooker
+docker run -d -p 8080:8080 --name edusnooker -e SPRING_PROFILES_ACTIVE=prod --network edusnooker-network edusnooker
