270ffc65e66cabadcdebacf8e33d06c7958732fc
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2022-10-05 23:21:19 +0200

    stripe part1

---
+++ src/main/java/pl/edusnooker/webapp/component/payment/PaymentController.java
@@ -0,0 +1,19 @@
+package pl.edusnooker.webapp.component.payment;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class PaymentController {
+
+    @PostMapping("checkout")
+    @PreAuthorize("hasAnyAuthority('user:demo')")
+    ResponseEntity<String>createCheckoutSession() {
+        return ResponseEntity.ok("ok - dostalem zapytanie");
+    }
+
+}
---
+++ src/main/java/pl/edusnooker/webapp/component/payment/PaymentService.java
@@ -0,0 +1,10 @@
+package pl.edusnooker.webapp.component.payment;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    String secretKey;
+}
