1211048921c4a907b020801222b58c5039c5e1d0
Łukasz Lebiecki <lukasz.lebiecki@gmail.com>
2022-10-08 22:56:04 +0200

    stripe part15

--- src/main/java/pl/edusnooker/webapp/component/payment/PaymentController.java
+++ src/main/java/pl/edusnooker/webapp/component/payment/PaymentController.java
@@ -74,9 +74,9 @@ public class PaymentController {
     @PostMapping("webhook/subscriptionCreate")
     public ResponseEntity<String> subscriptionCreate(@RequestBody StripeCreateSubscription stripeCreateSubscription) throws InterruptedException {
         System.out.println("teraz chcę opóźnić program");
-        Thread.sleep(9000);
-        System.out.println("program po 9 sekundach");
-        paymentService.setUserRole(stripeCreateSubscription.getData().getObject().getCustomer(), stripeCreateSubscription.getData().getObject().getCurrentPeriodEnd());
+        Thread.sleep(2000);
+        System.out.println("program po 2 sekundach");
+//        paymentService.setUserRole(stripeCreateSubscription.getData().getObject().getCustomer(), stripeCreateSubscription.getData().getObject().getCurrentPeriodEnd());
 
         return new ResponseEntity<>(gson.toJson(stripeCreateSubscription), HttpStatus.OK);
     }
